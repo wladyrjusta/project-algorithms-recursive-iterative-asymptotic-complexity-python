6de295978c829657ef648c3142e974fc9dabf453
wladyrjusta <wladyr.justa@gmail.com>
2023-08-21 15:05:54 -0300

    Requisito 1 completo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,20 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter: int = 0
+    if (
+        target_time is None or target_time == 0
+        or not isinstance(permanence_period, list)
+        or any(
+            not isinstance(period, tuple)
+            or len(period) != 2
+            or any(not isinstance(elemento, int) for elemento in period)
+            for period in permanence_period
+        )
+    ):
+        return None
+    for period in permanence_period:
+        if target_time in range(period[0], period[1] + 1):
+            counter += 1
+    return counter
+
+    # raise NotImplementedError
+
