86fbe451b7112e17b23c55665ae57e93e57ccc52
wladyrjusta <wladyr.justa@gmail.com>
2023-08-21 18:27:48 -0300

    Requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("ABCDE", 3) == "CBA_ED"
+    assert encrypt_message("ABC DE", 2) == "ED C_BA"
+    assert encrypt_message("ABCDE", -1) == "EDCBA"
+    assert encrypt_message("ABCDE", 9) == "EDCBA"
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(3, "hello world")
