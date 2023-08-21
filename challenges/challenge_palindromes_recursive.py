0025069677a38bc49b771b7e5b74d709ed554e8c
wladyrjusta <wladyr.justa@gmail.com>
2023-08-21 20:58:07 -0300

    Requisito 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "" or word[low_index] != word[high_index]:
+        return False
+    if high_index == 0:
+        return True
+    while low_index < len(word) - 1:
+        if word[low_index] == word[high_index]:
+            print(low_index, (len(word) - 1))
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return True
+
+    # raise NotImplementedError
