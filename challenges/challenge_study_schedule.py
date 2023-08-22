fab2494e6a04dfc8b734e4ff8ae1e412d6c3dce1
wladyrjusta <wladyr.justa@gmail.com>
2023-08-21 22:08:10 -0300

    Correção complexidade temporal Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,15 +3,15 @@ def study_schedule(permanence_period, target_time):
     if (
         target_time is None or target_time == 0
         or not isinstance(permanence_period, list)
-        or any(
-            not isinstance(period, tuple)
-            or len(period) != 2
-            or any(not isinstance(elemento, int) for elemento in period)
-            for period in permanence_period
-        )
     ):
         return None
     for period in permanence_period:
+        if (
+            not isinstance(period, tuple)
+            or len(period) != 2
+            or type(period[0]) is not int or type(period[1]) is not int
+        ):
+            return None
         if target_time in range(period[0], period[1] + 1):
             counter += 1
     return counter
