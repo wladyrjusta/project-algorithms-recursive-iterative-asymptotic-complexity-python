83481dc133a189f92a2f8ae0713c574497414e4c
wladyrjusta <wladyr.justa@gmail.com>
2023-08-21 21:00:06 -0300

    #

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -5,7 +5,6 @@ def is_palindrome_recursive(word, low_index, high_index):
         return True
     while low_index < len(word) - 1:
         if word[low_index] == word[high_index]:
-            print(low_index, (len(word) - 1))
             return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     return True
 
