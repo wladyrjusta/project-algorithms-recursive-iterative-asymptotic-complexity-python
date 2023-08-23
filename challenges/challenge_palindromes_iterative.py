50eb7c11d9e51de698d00cc750a75aa163f7da47
wladyrjusta <wladyr.justa@gmail.com>
2023-08-23 12:28:39 -0300

    Requisito 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,9 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "" or word is None:
+        return False
+    word = word.lower()
+    if word[::-1] == word:
+        return True
+    else:
+        return False
+    # raise NotImplementedError
