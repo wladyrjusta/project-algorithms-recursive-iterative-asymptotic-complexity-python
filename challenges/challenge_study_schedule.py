69f9bcf9ca5e5fc0e4e0faecddbaf1f9017f52d4
wladyrjusta <wladyr.justa@gmail.com>
2023-08-26 15:31:13 -0300

    #

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    counter: int = 0
     if (
         target_time is None or target_time == 0
         or not isinstance(permanence_period, list)
     ):
         return None
+    counter = 0
     for period in permanence_period:
         if (
             not isinstance(period, tuple)
@@ -12,7 +12,7 @@ def study_schedule(permanence_period, target_time):
             or type(period[0]) is not int or type(period[1]) is not int
         ):
             return None
-        if target_time in range(period[0], period[1] + 1):
+        if period[0] <= target_time <= period[1]:
             counter += 1
     return counter
 
