1b6ab87f18a561730c1765ef5eea556a7df9aa4d
wladyrjusta <wladyr.justa@gmail.com>
2023-08-26 15:10:09 -0300

    Requisito 4 complexidade assintótica ajustada para no max O(n log n)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,29 +1,36 @@
 def is_anagram(first_string: str, second_string: str) -> (str, str, bool):
+    ordered_first_string = list(first_string.lower())
+    string_merge_sort(
+        ordered_first_string, 0, len(ordered_first_string)
+    )
+    ordered_second_string = list(second_string.lower())
+    string_merge_sort(
+        ordered_second_string, 0, len(ordered_second_string)
+    )
     if (
         first_string == ""
         or second_string == ""
         or len(first_string) != len(second_string)
     ):
         return (
-            string_ordner(first_string.lower()),
-            string_ordner(second_string.lower()),
+            "".join(ordered_first_string),
+            "".join(ordered_second_string),
             False,
         )
     first_string_char_counter = char_counter(first_string, second_string)
-    second_string_char_counter = char_counter(second_string, first_string)
-    if first_string_char_counter == second_string_char_counter:
+    second_string_cahr_counter = char_counter(second_string, first_string)
+    if first_string_char_counter == second_string_cahr_counter:
         return (
-            string_ordner(first_string.lower()),
-            string_ordner(second_string.lower()),
+            "".join(ordered_first_string),
+            "".join(ordered_second_string),
             True,
         )
     else:
         return (
-            string_ordner(first_string.lower()),
-            string_ordner(second_string.lower()),
+            "".join(ordered_first_string),
+            "".join(ordered_second_string),
             False,
         )
-    # raise NotImplementedError
 
 
 def char_counter(first_string, second_string):
@@ -40,15 +47,32 @@ def char_counter(first_string, second_string):
     return char_counter
 
 
-def string_ordner(string: str) -> str:
-    new_string = list(string)
-    list_length = len(new_string)
-    for index in range(list_length - 1):
-        min_letter_index = index
-        for search_index in range(index + 1, list_length):
-            if new_string[search_index] < new_string[min_letter_index]:
-                min_letter_index = search_index
-        curr_char = new_string[index]
-        new_string[index] = new_string[min_letter_index]
-        new_string[min_letter_index] = curr_char
-    return "".join(new_string)
+def string_merge_sort(string: [str], start=0, end=None) -> [str]:
+    if end is None:
+        end = len(string)
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        string_merge_sort(string, start, mid)
+        string_merge_sort(string, mid, end)
+        merge_string(string, start, mid, end)
+
+
+def merge_string(string: [str], start: int, mid: int, end: int):
+    left = string[start:mid]
+    right = string[mid:end]
+
+    left_index, right_index = 0, 0
+
+    for general_index in range(start, end):
+        if left_index >= len(left):
+            string[general_index] = right[right_index]
+            right_index += 1
+        elif right_index >= len(right):
+            string[general_index] = left[left_index]
+            left_index += 1
+        elif left[left_index] < right[right_index]:
+            string[general_index] = left[left_index]
+            left_index += 1
+        else:
+            string[general_index] = right[right_index]
+            right_index += 1
